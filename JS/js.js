46f779d11403a9c1cff02a0182610b7b59095e3b
AyoubDev-1 <92212231+AyoubDev-1@users.noreply.github.com>
2021-11-27 11:33:50 +0100

    Update js.js

--- JS/js.js
+++ JS/js.js
@@ -218,7 +218,7 @@ const countEl = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/ayoubdriri/portfolio?amount=1')
+	fetch('https://api.countapi.xyz/update/AyoubFolio/Portfolio?amount=1')
 	.then(res => res.json())
 	.then(res => {
 		countEl.innerHTML = res.value;
